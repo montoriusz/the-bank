06466be4765e2ced9481432fe721c1c9e17ff5a1
Pawel Motofa <pawel.motofa@ormdigital.com>
2023-09-28 12:13:50 +0200

    add clerk

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { ThemeProvider } from "~/components/ThemeProvider";
 
@@ -29,11 +30,13 @@ export default function Layout(props: { children: React.ReactNode }) {
       className={`${roboto_mono.variable} font-mono`}
       suppressHydrationWarning
     >
-      <body suppressHydrationWarning={true}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {props.children}
-        </ThemeProvider>
-      </body>
+      <ClerkProvider>
+        <body suppressHydrationWarning={true}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {props.children}
+          </ThemeProvider>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
